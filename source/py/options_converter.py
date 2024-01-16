b27df20a0b5a131d7a8522de2c7f5294b9b6ec28
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-16 21:17:44 +0100

    Added output file support for the python version

--- source/py/options_converter.py
+++ source/py/options_converter.py
@@ -1,6 +1,6 @@
 import sys
 
-def modify_file(file_path):
+def modify_file(file_path, output_file_path):
     target_bytes = bytes([0xD8, 0x05, 0x20, 0x20, 0x6D, 0x70])
     with open(file_path, 'rb') as file:
         content = file.read()
@@ -8,7 +8,7 @@ def modify_file(file_path):
         if content.startswith(target_bytes):
             modified_content = content.replace(b'\x20', b'\x00')
 
-            with open(file_path, 'wb') as modified_file:
+            with open(output_file_path, 'wb') as modified_file:
                 modified_file.write(modified_content)
             print("Modification successful.")
         else:
@@ -19,8 +19,10 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 2:
         file_path = sys.argv[1]
-    if len(sys.argv) > 2:
-        print("Too many arguments.")
-        exit(1)
 
-    modify_file(file_path)
+    output_file_path = file_path
+
+    if len(sys.argv) == 3:
+        output_file_path = sys.argv[2]
+
+    modify_file(file_path, output_file_path)
