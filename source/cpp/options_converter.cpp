1ae3ee606e02e20e5687d0c88e9f3f0d381cc5d8
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-17 14:41:57 +0100

    Added output file path argument for the cpp converter

--- source/cpp/options_converter.cpp
+++ source/cpp/options_converter.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <cstdint>
 
-void modifyFile(const std::string& filePath) {
+void modifyFile(const std::string& filePath, const std::string& outputFilePath) {
     std::vector<uint8_t> targetBytes = {0xD8, 0x05, 0x20, 0x20, 0x6D, 0x70};
     std::ifstream file(filePath, std::ios::binary);
     if (file) {
@@ -14,7 +14,7 @@ void modifyFile(const std::string& filePath) {
             std::equal(targetBytes.begin(), targetBytes.end(), content.begin())) {
 
             std::replace(content.begin(), content.end(), static_cast<uint8_t>(0x20), static_cast<uint8_t>(0x00));
-            std::ofstream modifiedFile(filePath, std::ios::binary);
+            std::ofstream modifiedFile(outputFilePath, std::ios::binary);
             
             modifiedFile.write(reinterpret_cast<const char*>(content.data()), content.size());
             std::cout << "Modification successful." << std::endl;
@@ -28,14 +28,17 @@ void modifyFile(const std::string& filePath) {
 
 int main(int argc, char *argv[]) {
     std::string filePath = "options.txt";
-    
-    if(argc == 2) filePath = argv[1];
-    if(argc > 2){
+    std::string outputFilePath = filePath;
+
+    if(argc >= 2) filePath = argv[1];
+    if(argc == 3) outputFilePath = argv[2];
+
+    if(argc > 3){
         std::cerr << "Too many arguments." << std::endl;
         return 1;
     }
 
-    modifyFile(filePath);
+    modifyFile(filePath, outputFilePath);
 
     return 0;
 }
