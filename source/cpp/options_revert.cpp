b321c5f1f44c4c836530eaedea6493bfc5a11533
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-17 15:30:30 +0100

    Match original coding style

--- source/cpp/options_revert.cpp
+++ source/cpp/options_revert.cpp
@@ -49,10 +49,10 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if(argc >= 2) filePath = argv[1];
-    if(argc == 3) outputFilePath = argv[2];
+    if (argc >= 2) filePath = argv[1];
+    if (argc == 3) outputFilePath = argv[2];
 
-    if(argc > 3){
+    if (argc > 3){
         std::cerr << "Too many arguments." << std::endl;
         return 1;
     }
